a46c10ffe10648c90e1b650c615415818be3b210
Jason McCreary <jason@pureconcepts.net>
2020-05-04 08:50:31 -0400

    Redo homepage content

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -62,6 +62,10 @@
                         @include('_nav.search-input')
                     @endif
                 </div>
+
+                <div class="hidden lg:flex lg:flex-1 items-center justify-end">
+                  <a href="https://github.com/laravel-shift/blueprint" title="Blueprint on GitHub" class="flex"><img src="/assets/img/logo-github.png" alt="GitHub Octocat logo"></a>
+                </div>
             </div>
 
             @yield('nav-toggle')
--- source/index.blade.php
+++ source/index.blade.php
@@ -8,12 +8,11 @@
 
             <h2 id="intro-powered-by-jigsaw" class="font-light mt-4">{{ $page->siteDescription }}</h2>
 
-            <p class="text-lg">Give your documentation a boost with Jigsaw. <br class="hidden sm:block">Generate elegant, static docs quickly and easily.</p>
+            <p class="text-lg"><b>Rapidly develop</b> multiple Laravel components<br>from a <b>single, human readable</b> domain laungage.</p>
 
             <div class="flex my-10">
-                <a href="/docs/getting-started" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
-
-                <a href="https://jigsaw.tighten.co" title="Jigsaw by Tighten" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">About Jigsaw</a>
+                <a href="/docs/getting-started" title="Blueprint documentation" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
+                <a href="https://laracasts.com/series/guest-spotlight/episodes/9" title="Blueprint spotlight on Laracasts" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6" rel="external">Watch an Overview</a>
             </div>
         </div>
 
@@ -25,26 +24,20 @@
     <div class="md:flex -mx-2 -mx-4">
         <div class="mb-8 mx-3 px-2 md:w-1/3">
             <img src="/assets/img/icon-window.svg" class="h-12 w-12" alt="window icon">
-
-            <h3 id="intro-laravel" class="text-2xl text-blue-900 mb-0">Templating with <br>Laravel's Blade engine</h3>
-
-            <p>Blade is a powerful, simple, and beautiful templating language, and now you can use it for your static sites, not just your Laravel-powered apps.</p>
+            <h3 class="text-2xl text-blue-900 mb-0">Draft your application<br>with a simple syntax</h3>
+            <p>Blueprint uses a simple YAML syntax to define your <i>models</i> and <i>controllers</i> offering shorthands and leveraging conventions to maximize the developer experience.</p>
         </div>
 
         <div class="mb-8 mx-3 px-2 md:w-1/3">
             <img src="/assets/img/icon-terminal.svg" class="h-12 w-12" alt="terminal icon">
-
-            <h3 id="intro-markdown" class="text-2xl text-blue-900 mb-0">Use Markdown for <br>content-driven pages</h3>
-
-            <p>Markdown is the web’s leading format for writing articles, blog posts, documentation, and more. Jigsaw makes it painless to work with Markdown content.</p>
+            <h3 class="text-2xl text-blue-900 mb-0">Generate code using<br>artisan commands</h3>
+            <p>Blueprint comes with artisan commands making it easy and familiar to build new components and reference existing within your Laravel application.</p>
         </div>
 
         <div class="mx-3 px-2 md:w-1/3">
             <img src="/assets/img/icon-stack.svg" class="h-12 w-12" alt="stack icon">
-
-            <h3 id="intro-mix" class="text-2xl text-blue-900 mb-0">Compile your assets <br>using Laravel Mix </h3>
-
-            <p>Jigsaw comes pre-configured with Laravel Mix, a simple and powerful build tool. Use the latest frontend tech with just a few lines of code.</p>
+            <h3 class="text-2xl text-blue-900 mb-0">Output multiple Laravel<br>components at once</h3>
+            <p>Blueprint not only generates <i>models</i> and <i>controllers</i>, but factories, migrations, form requests, events, jobs, and mailables. Oh, <b>and tests too</b>.</p>
         </div>
     </div>
 </section>
