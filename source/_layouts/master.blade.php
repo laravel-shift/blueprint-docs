916b4dbcfa53667a1d5f7cc766ddb60b647f3fe2
Jason McCreary <jason@pureconcepts.net>
2020-05-03 20:45:18 -0400

    Add Google Analytics

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -28,7 +28,14 @@
         @stack('meta')
 
         @if ($page->production)
-            <!-- Insert analytics code here -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-71657594-3"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-71657594-3');
+        </script>
         @endif
 
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
