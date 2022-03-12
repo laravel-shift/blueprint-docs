afdfd920f3a47b35364e39b6b094b0bdb015fbf2
Jason McCreary <jason@pureconcepts.net>
2022-03-12 18:51:24 -0500

    Migrate to nginx server

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 vendor/bin/jigsaw build production
 npm run production
-rsync -v -rz --checksum --delete build_production/ web:/var/www/blueprint.laravelshift.com/public
+rsync -v -rz --checksum --delete build_production/ nginx:/var/www/blueprint.laravelshift.com/html
 rm -rf build_production
