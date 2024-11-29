2e76ba441bce5e9278a1210d2bd5fefe7049f9f8
Jason McCreary <jason@pureconcepts.net>
2024-11-29 14:17:28 -0500

    Deploy to Cloudflare Pages

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-vendor/bin/jigsaw build production
 npm run production
-rsync -v -rz --checksum --delete build_production/ nginx:/var/www/blueprint.laravelshift.com/html
-rm -rf build_production
+vendor/bin/jigsaw build --quiet production
+#git add build/
+#git commit -m 'Deploy'
+#git push origin HEAD
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,22 +1,9 @@
 let mix = require('laravel-mix');
-let build = require('./tasks/build.js');
 let tailwindcss = require('tailwindcss');
 require('laravel-mix-purgecss');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build/');
-mix.webpackConfig({
-    plugins: [
-        build.jigsaw,
-        build.browserSync(),
-        build.watch([
-            'config.php',
-            'source/**/*.md',
-            'source/**/*.php',
-            'source/**/*.scss',
-        ]),
-    ],
-});
 
 mix.js('source/_assets/js/main.js', 'js')
     .sourceMaps()
