9fce8dcbc08d03ab2c81ade63851f08d2c101330
Jason McCreary <jason@pureconcepts.net>
2024-12-01 17:40:54 -0500

    Tailwind 2.x Shift (#34)

--- source/_nav/search-input.blade.php
+++ source/_nav/search-input.blade.php
@@ -1,6 +1,6 @@
 <div id="js-search-input" class="docsearch-input__wrapper hidden md:block relative">
     <label for="search" class="hidden">Search</label>
-    <input id="docsearch-input" name="docsearch" class="docsearch-input transition-colors duration-100 ease-in-out focus:outline-0 border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-600 rounded-lg bg-gray-200 py-2 pr-4 pl-6 block w-full appearance-none leading-normal" type="text" placeholder="search the docs (press &quot;/&quot; to focus)">
+    <input id="docsearch-input" name="docsearch" class="docsearch-input transition-colors duration-100 ease-in-out focus:outline-0 border border-transparent focus:bg-white focus:border-gray-200 placeholder-gray-500 rounded-lg bg-gray-100 py-2 pr-4 pl-6 block w-full appearance-none leading-normal" type="text" placeholder="search the docs (press &quot;/&quot; to focus)">
 
     <button class="md:hidden absolute pin-t pin-r h-full font-light text-3xl text-blue-500 hover:text-blue-600 focus:outline-none -mt-px pr-7">&times;</button>
 </div>
